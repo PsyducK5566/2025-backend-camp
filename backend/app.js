eb003f2d6ed1f3f884ff608319712c04994a84d5
psyduck5566 <snowdrinking@gmail.com>
2026-01-27 02:13:10 +0800

    Fix: 修改健康檢查回傳格式

--- backend/app.js
+++ backend/app.js
@@ -30,10 +30,10 @@ app.use(
 app.use(express.static(path.join(__dirname, "public")));
 
 // 修改健康檢查回傳格式
-app.get("/healthcheck", (req, res) => {
+app.get('/healthcheck', (req, res) => {
   res.status(200).json({
-    status: "success",
-    message: "OK"
+    status: 'success',
+    message: 'OK'
   })
 })
 app.use("/api/credit-package", creditPackageRouter);
